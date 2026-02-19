6b6c18543f32e106eb2a2090eea95e42efd58593
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-19 11:41:25 +0100

    update

--- c01/ex02/ft_swap.c
+++ c01/ex02/ft_swap.c
@@ -1,9 +1,9 @@
 /**
- * ft_swap: swap numbers
- * 
+ * ft_swap - swap numbers
+ *
  * @a: pointer to first number
  * @b: pointer to second number
- * 
+ *
  * Return: 0
  */
 void ft_swap(int *a, int *b)
