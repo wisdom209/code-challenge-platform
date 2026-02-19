d279de37fa3b77f6ddbe6c2682783bf53f71db1c
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-19 03:02:28 +0100

    update

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -10,16 +10,16 @@ void ft_print_comb(void)
 	int x, y, z, c;
 
 	c = ' ';
-	for (x = '9'; x >= '0'; x--)
+	for (x = '0'; x <= '9'; x++)
 	{
-		for (y = '8'; y >= '0'; y--)
+		for (y = '1'; y <= '8'; y++)
 		{
-			for (z = '7'; z >= '0'; z--)
+			for (z = '2'; z <= '7'; z++)
 			{
 				write(1, &x, 1);
 				write(1, &y, 1);
 				write(1, &z, 1);
-				if (!((z == '0') & (y == '0') & (x == '0')))
+				if (!((x == '9') && (y == '8') && (z == '7')))
 					write(1, &c, 1);
 			}
 		}
