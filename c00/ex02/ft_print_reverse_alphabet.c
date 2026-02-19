e22d9ef8d3e0845c79eaed85fb3b8916ec3e3dd4
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-19 02:06:41 +0100

    update

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * ft_print_reverse_alphabet - entry
+ *
+ * Return: void
+ */
+void ft_print_reverse_alphabet(void)
+{
+	int i;
+
+	for (i = 'z', i >= 'a'; i--)
+		write(1, &i, 1);
+}
