2a14611cc02cc3da550bdb71f2c6da4c8392d325
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-20 08:48:51 +0100

    update

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * ft_putstr - entry
+ * @str: string
+ *
+ * Return: void
+ */
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
