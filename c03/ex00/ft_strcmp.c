777371966cdf815c832f9219648d5d706a9b0abf
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-21 12:04:09 +0100

    update

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -13,12 +13,10 @@ int ft_strcmp(char *s1, char *s2)
 
 	while (1)
 	{
-		if (s1[i] < s2[i])
-			return (-1);
-		else if (s1[i] > s2[i])
-			return (1);
-		else if (s1[i] == '\0' && s2[i] == '\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 			return (0);
+		if ((s1[i] - s2[i]) != 0)
+			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
