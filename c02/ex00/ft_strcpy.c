6d494ba1653b2650ce8c2b8e5206e95ae0135cd3
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-25 11:18:10 +0100

    update

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -9,8 +9,9 @@ char *ft_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-
+		
+	dest[i] = '\0';
 	return (dest);
 }
