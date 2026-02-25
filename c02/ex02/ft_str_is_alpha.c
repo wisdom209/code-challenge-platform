03883d3f4838181a95630e5c9f1f287e535eb3c7
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-25 12:53:49 +0100

    update

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,22 @@
+/**
+ * ft_str_is_alpha - check if string contains only alphabets
+ * @str: string
+ *
+ * Return: int
+ */
+int ft_str_is_alpha(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z') && !(str[i] >= 'A' && str[i] <= 'Z'))
+			return (0);
+
+		i++;
+	}
+
+	return (1);
+}
