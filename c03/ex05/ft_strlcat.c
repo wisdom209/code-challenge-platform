31c2af1a300e5d1704b2e1a819fc2f62bfac6823
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-25 05:36:10 +0100

    update

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,13 +1,52 @@
 /**
+ * ft_strlen - string length
+ * @str: string to count
+ *
+ * Return: int
+ */
+int ft_strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
  * ft_strlcat - man strlcat
  * @dest: string
  * @src: string
  * @size: unsigned int
- * 
+ *
  * Return: unsigned int
  */
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	
+	unsigned int dest_len, src_len, i, j, size_to_add;
 
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	size_to_add = size - dest_len - 1;
+
+	if (size <= dest_len)
+		return (size + src_len);
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		if (j < size_to_add)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+		else
+			break;
+	}
+
+	dest[i] = '\0';
+
+	return (dest_len + src_len);
 }
