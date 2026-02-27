3916f7a2f7dfdb916aa669d20f4045b1e40ec592
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-27 17:25:59 +0100

    update

--- c02/ex08/ft_strlowcase.c
+++ c02/ex08/ft_strlowcase.c
@@ -10,7 +10,7 @@ char *ft_strlowcase(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] = str[i] + 32;
 	}
 	return (str);
