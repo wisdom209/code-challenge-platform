aaa6875a505d498ea21ceba9a8bb67dd9f88c008
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-27 18:05:53 +0100

    update

---
+++ c02/ex10/README.md
@@ -0,0 +1,9 @@
+Turn-in directory: c02/ex10/
+
+Files to turn in: ft_strlcpy.c
+
+Allowed functions: none specified
+
+Prototype: unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
+
+Description: Reproduce the behavior of strlcpy (man strlcpy).
---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,35 @@
+/**
+ * ft_strlen - string length
+ * @str: string
+ *
+ * Return: int
+ */
+int ft_strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * ft_strlcpy - entry
+ * @dest: string
+ * @src: string
+ * @size: number
+ *
+ * Return: number
+ */
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i, src_size;
+
+	i = 0;
+	src_size = ft_strlen(src);
+
+	for (i = 0; (i < size - 1 && i < src_size); i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (src_size);
+}
