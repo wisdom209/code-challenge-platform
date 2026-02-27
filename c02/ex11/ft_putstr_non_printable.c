c304da30d42b7a864b1fa056b8e049360d91d334
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-27 22:35:56 +0100

    update

---
+++ c02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,31 @@
+#include "unistd.h"
+
+/**
+ * ft_putstr_non_printable - entry
+ * @str: string
+ *
+ * Return: void
+ */
+void ft_putstr_non_printable(char *str)
+{
+	int i, first, second;
+	char *letters;
+
+	letters = "0123456789abcdef";
+	i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] <= 32)
+		{
+			write(1, "\\x", 3);
+			first = (int)str[i] / 16;
+			second = (int)str[i] % 16;
+
+			write(1, &letters[first], 1);
+			write(1, &letters[second], 1);
+		}
+		else
+			write(1, &str[i], 1);
+	}
+}
