6828d2d49aa2fecad69794c7e38490e503522122
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-02-27 18:15:56 +0100

    update

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -28,8 +28,15 @@ unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
 	i = 0;
 	src_size = ft_strlen(src);
 
+	if (size == 0)
+	{
+		dest[0] = '\0';
+		return (src_size);
+	}
+
 	for (i = 0; (i < size - 1 && i < src_size); i++)
 		dest[i] = src[i];
 
+	dest[i] = '\0';
 	return (src_size);
 }
