cabbbc5a535ab5c9211f123816b9d0dfcf73dd86
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-07 10:49:05 +0100

    update

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+/**
+ * ft_putnbr - writes a number to stdout
+ * @nb: number
+ *
+ * Return: void
+ */
+void ft_putnbr(int nb)
+{
+	int buffer[20], i, j;
+	char c;
+
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = nb * -1;
+	}
+
+	if (nb == 0)
+	{
+		write(1, "0", 1);
+		return;
+	}
+
+	for (i = 0; nb > 0; i++)
+	{
+		buffer[i] = nb % 10;
+		nb = nb / 10;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		c = buffer[j] + '0';
+		write(1, &c, 1);
+	}
+}
