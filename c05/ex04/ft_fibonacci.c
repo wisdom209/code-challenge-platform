91fc6447d8b9346538b542af07840cd16e28ac96
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-12 16:55:10 +0100

    update

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,16 @@
+/**
+ * ft_fibonacci - entry
+ * @index: index of number
+ *
+ * Return: int
+ */
+int ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index <= 2)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
