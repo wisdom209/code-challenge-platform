ddf13fa1db21a3951867c8a4d9d1eed9b3c1efbc
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-15 06:19:37 +0100

    update

---
+++ c06/ex03/README.md
@@ -0,0 +1,9 @@
+Turn-in directory: c06/ex03/
+
+Files to turn in: ft_sort_params.c
+
+Allowed functions: write
+
+Prototype: int main(int argc, char **argv);
+
+Description: Create a program that displays its given arguments sorted by ascii order. It should display all arguments, except for argv[0]. One argument per line.
---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * ft_swap - swap strings
+ * @a: string
+ * @b: string
+ *
+ * Return: void
+ */
+void ft_swap(char **a, char **b)
+{
+	char *tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * ft_strcmp - compare strings
+ * @a: string
+ * @b: string
+ *
+ * Return: int
+ */
+int ft_strcmp(char *a, char *b)
+{
+	int i;
+
+	i = 0;
+
+	while (a[i] && b[i] && (a[i] == b[i]))
+		i++;
+
+	return (a[i] - b[i]);
+}
+
+/**
+ * main - entry
+ * @argc: arg count
+ * @argv: arg number
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int i, j, buffer_len;
+	char *buffer[1024];
+
+	if (argc < 2)
+		return (0);
+
+	for (i = 1; i < argc; i++)
+		buffer[i - 1] = argv[i];
+
+	buffer[i] = '\0';
+	buffer_len = argc - 1;
+
+	for (i = 0; i < buffer_len - 1; i++)
+	{
+		for (j = 0; j < buffer_len - 1; j++)
+		{
+			if (ft_strcmp(buffer[j], buffer[j + 1]) > 0)
+				ft_swap(&buffer[j], &buffer[j + 1]);
+		}
+	}
+
+	for (i = 0; i < argc - 1; i++)
+		printf("%s\n", buffer[i]);
+
+	return (0);
+}
