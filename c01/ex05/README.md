01f144e78a84a1d22cb81281d392c93fad59b6a8
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-21 00:38:25 +0100

    update

---
+++ c01/ex05/README.md
@@ -0,0 +1,9 @@
+Turn-in directory: c01/ex05/
+
+Files to turn in: ft_putstr.c
+
+Allowed functions: write
+
+Prototype: void ft_putstr(char *str);
+
+Description: Displays a string of characters on the standard output.
---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wisdom <ononiwuwisdom@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/21 00:35:34 by wisdom            #+#    #+#             */
+/*   Updated: 2026/03/21 00:38:12 by wisdom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+/**
+ * ft_putstr - Displays a string of characters
+ * on the standard output.
+ * @str: string of characters
+ *
+ * Return: void
+ */
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+	}
+}
