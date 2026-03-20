29a1f92acec7389c8b22f4cc25947fe3edab2d6e
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-20 22:54:07 +0100

    update

---
+++ c00/ex05/README.md
@@ -0,0 +1,11 @@
+Turn-in directory: c00/ex05/
+
+Files to turn in: ft_print_comb.c
+
+Allowed functions: write
+
+Prototype: void ft_print_comb(void);
+
+Description: Display all different combinations of three different digits in ascending order.
+
+Forbidden functions: printf, puts
---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wisdom <ononiwuwisdom@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/20 22:41:12 by wisdom            #+#    #+#             */
+/*   Updated: 2026/03/20 22:52:57 by wisdom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+/**
+ * ft_print_comb - Display all different combinations of
+ * three different digits in ascending order
+ *
+ * Return: void
+ */
+void	ft_print_comb(void)
+{
+	char	x;
+	char	y;
+	char	z;
+
+	x = '0';
+	while (x <= '9')
+	{
+		y = x + 1;
+		while (y <= '9')
+		{
+			z = y + 1;
+			while (z <= '9')
+			{
+				write(1, &x, 1);
+				write(1, &y, 1);
+				write(1, &z, 1);
+				if (x == '7' && y == '8' && z == '9')
+					break ;
+				write(1, " ", 1);
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+}
