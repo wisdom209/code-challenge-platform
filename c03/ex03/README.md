789f9b2cb41143bc789360cd531a392b69b915a0
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-22 05:23:43 +0100

    update

---
+++ c03/ex03/README.md
@@ -0,0 +1,9 @@
+Turn-in directory: c03/ex03/
+
+Files to turn in: ft_strncat.c
+
+Allowed functions: none specified
+
+Prototype: char *ft_strncat(char *dest, char *src, unsigned int nb);
+
+Description: Reproduce the behavior of strncat (man strncat). Appends at most nb characters from src to dest.
---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wisdom <ononiwuwisdom@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/22 05:19:11 by wisdom            #+#    #+#             */
+/*   Updated: 2026/03/22 05:23:31 by wisdom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * ft_strncat - Reproduce the behaviour of strncat
+ * @dest: destination string
+ * @src: source string
+ * @nb: number of bytes to append
+ *
+ * Return: destination string
+ */
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j] && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
