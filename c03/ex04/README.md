ff42f79a90155c2c9b54f36ea63ad82a5d290969
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-22 05:47:21 +0100

    update

---
+++ c03/ex04/README.md
@@ -0,0 +1,9 @@
+Turn-in directory: c03/ex04/
+
+Files to turn in: ft_strstr.c
+
+Allowed functions: none specified
+
+Prototype: char *ft_strstr(char *str, char *to_find);
+
+Description: Reproduce the behavior of strstr (man strstr). Finds the first occurrence of the substring to_find in str. Returns a pointer to the beginning of the located substring, or NULL if not found. In these tests, the return value is given as the offset from the start of str (or -1 for NULL).
---
+++ c03/ex04/ft_strstr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wisdom <ononiwuwisdom@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/22 05:26:40 by wisdom            #+#    #+#             */
+/*   Updated: 2026/03/22 05:47:15 by wisdom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_substr(char *str, char *substr)
+{
+	int	i;
+	int	substr_len;
+
+	i = 0;
+	substr_len = 0;
+	while (substr[substr_len])
+		substr_len++;
+	while (i < substr_len)
+	{
+		if (!str[i] || str[i] != substr[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * ft_strstr - Reproduce behaviour of strstr
+ * @str: source string
+ * @to_find: substring to find in str
+ *
+ * Return: point to located substring
+ */
+char	*ft_strstr(char *str, char *to_find)
+{
+	while (*str)
+	{
+		if (ft_is_substr(str, to_find) == 1)
+			return (str);
+		str++;
+	}
+	return (0);
+}
