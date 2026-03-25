e57cfac457dcc92dbbdf6f4c21d5dbb60486c7bf
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-25 06:11:07 +0100

    update

---
+++ c07/ex01/README.md
@@ -0,0 +1,9 @@
+Turn-in directory: c07/ex01/
+
+Files to turn in: ft_range.c
+
+Allowed functions: malloc
+
+Prototype: int *ft_range(int min, int max);
+
+Description: Returns an array of ints containing all values between min and max (min included, max excluded).
---
+++ c07/ex01/ft_range.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wisdom <ononiwuwisdom@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/25 06:06:39 by wisdom            #+#    #+#             */
+/*   Updated: 2026/03/25 06:10:59 by wisdom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+/**
+ * ft_range - Display ints between min and max
+ * @min: minimum number (included)
+ * @max: maximum number (excluded)
+ *
+ * Return: array of integers
+ */
+int	*ft_range(int min, int max)
+{
+	int	*range;
+	int	i;
+
+	range = malloc(max - min);
+	i = 0;
+	while (i < (max - min))
+	{
+		range[i] = min + i;
+		i++;
+	}
+	return (range);
+}
