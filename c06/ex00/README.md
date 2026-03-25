4ccf33aa1df887ccfb0c7b2961c1ecd6a01376e9
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-25 04:49:21 +0100

    update

---
+++ c06/ex00/README.md
@@ -0,0 +1,9 @@
+Turn-in directory: c06/ex00/
+
+Files to turn in: ft_print_program_name.c
+
+Allowed functions: write
+
+Prototype: int main(int argc, char **argv);
+
+Description: Create a program that displays its own name.
---
+++ c06/ex00/ft_print_program_name.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wisdom <ononiwuwisdom@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/25 04:43:45 by wisdom            #+#    #+#             */
+/*   Updated: 2026/03/25 04:48:51 by wisdom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+/**
+ * ft_print_program_name - display name of program
+ * @argc: number of command line arguments plus program name
+ * @argv: program name and command line argument array
+ *
+ * Return: int
+ */
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[0][i])
+	{
+		write(1, &argv[0][i], 1);
+		i++;
+	}
+	return (0);
+}
