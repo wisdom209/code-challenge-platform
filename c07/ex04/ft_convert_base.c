2bbe8446d7cf46cc0e8140d9acbdc8812649b1aa
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-28 17:52:47 +0100

    update

---
+++ c07/ex04/ft_convert_base.c
@@ -0,0 +1,163 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wisdom <ononiwuwisdom@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/28 14:44:48 by wisdom            #+#    #+#             */
+/*   Updated: 2026/03/28 17:50:35 by wisdom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * get_base_len - finds the length of base if valid
+ * @base: base string
+ *
+ * Return: base_length if valid, else -1
+ */
+int	get_base_len(char *base)
+{
+	int	base_len;
+	int	i;
+
+	base_len = 0;
+	i = 0;
+	while (base[base_len])
+	{
+		if (base[base_len] == '+' || base[base_len] == '-')
+			return (-1);
+		if (base[base_len] <= 32 || base[base_len] == 127)
+			return (-1);
+		i = base_len + 1;
+		while (base[i])
+		{
+			if (base[base_len] == base[i])
+				return (-1);
+			i++;
+		}
+		base_len++;
+	}
+	if (base_len <= 1)
+		return (-1);
+	return (base_len);
+}
+
+/**
+ * ft_str_contains_char - check if char is in string
+ * @c: char
+ * @str: string
+ *
+ * Return: index of char else -1;
+ */
+int	ft_str_contains_char(char c, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+/**
+ * ft_atoi - converts ascii to integer
+ * @nbr: number in ascii
+ * @base: base of number
+ *
+ * Return: valid integer
+ */
+int	ft_atoi(char *nbr, char *base)
+{
+	int	result;
+	int	sign;
+	int	char_index;
+
+	sign = 1;
+	while (*nbr < 32)
+		nbr++;
+	while (*nbr == '-' || *nbr == '+')
+	{
+		if (*nbr == '-')
+			sign *= -1;
+		nbr++;
+	}
+	result = 0;
+	while (*nbr)
+	{
+		char_index = ft_str_contains_char(*nbr, base);
+		if (char_index < 0)
+			break ;
+		result = (result * 10) + char_index;
+		nbr++;
+	}
+	return (result * sign);
+}
+
+/**
+ * ft_itoa - converts integer to ascii
+ * @nbr: integer
+ * @base_to: base to convert it to
+ * @is_neg: 1 if integer is negative else 0
+ *
+ * Return: base representation of nbr in base_to
+ */
+char	*ft_itoa(int nbr, char *base_to, int is_neg)
+{
+	char	buffer[1024];
+	char	*int_repr;
+	int		i;
+
+	i = 0;
+	if (nbr == 0)
+		buffer[i++] = base_to[0];
+	while (nbr > 0)
+	{
+		buffer[i++] = base_to[nbr % get_base_len(base_to)];
+		nbr /= get_base_len(base_to);
+		if (nbr <= 0 && is_neg == 1)
+			buffer[i++] = '-';
+	}
+	int_repr = malloc(sizeof(char) * (i));
+	if (!int_repr)
+		return (NULL);
+	int_repr[i--] = '\0';
+	nbr = 0;
+	while (i >= 0)
+		int_repr[nbr++] = buffer[i--];
+	return (int_repr);
+}
+
+/**
+ * ft_convert_base - Converts nbr from base_from to base_to
+ * @nbr: number
+ * @base_from: source number base
+ * @base_to: destination number base_to
+ *
+ * Return: nbr in base_to else NULL if base is invalid
+ */
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int		nbr_to_int;
+	char	*base_conversion;
+	int		is_neg;
+
+	if (get_base_len(base_from) < 0 || get_base_len(base_to) < 0)
+		return (NULL);
+	nbr_to_int = ft_atoi(nbr, base_from);
+	is_neg = 0;
+	if (nbr_to_int < 0)
+	{
+		nbr_to_int *= -1;
+		is_neg = 1;
+	}
+	base_conversion = ft_itoa(nbr_to_int, base_to, is_neg);
+	return (base_conversion);
+}
