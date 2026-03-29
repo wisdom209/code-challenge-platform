e76e4e70b1dbfbd2c1c3381bfca6ca0d02e83a8d
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2026-03-29 06:37:54 +0100

    update

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wisdom <ononiwuwisdom@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/29 06:02:40 by wisdom            #+#    #+#             */
+/*   Updated: 2026/03/29 06:36:06 by wisdom           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define TRUE 1
+
+# define FALSE 0
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+# define SUCCESS 0
+
+static inline int	even(int nbr)
+{
+	if (nbr % 2 == 0)
+		return (1);
+	return (0);
+}
+typedef int	t_bool;
+void	ft_putstr(char *str);
+t_bool	ft_is_even(int nbr);
+#endif
